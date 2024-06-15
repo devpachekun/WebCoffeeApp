dd3a14ac51105fd2e2b35b6b379bf9ef904c3a98
Cristobal Pacheco <cristobalpacheco07@gmail.com>
2024-06-15 17:21:09 -0400

    Logo + Anadir Font de Google Fonts

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-[#B2BCAA] p-4">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
-          <div className="text-white text-xl">Pacheco's Coffee Shop</div>
+          <div className="flex items-center gap-5">
+            <img src="/logo_pachecos_coffee_shop.png" alt="Club de Tenis Graneros" className="h-12" />
+            <div className="text-white text-xl">Pacheco's Coffee Shop</div>
+          </div>
           <div>
-            <a href="#" className="text-gray-300 hover:text-white mx-2">Inicio</a>
-            <a href="#" className="text-gray-300 hover:text-white mx-2">Coffee</a>
-            <a href="#" className="text-gray-300 hover:text-white mx-2">Acerca de</a>
-            <a href="#" className="text-gray-300 hover:text-white mx-2">Iniciar Sesión</a>
-            <a href="#" className="text-gray-300 hover:text-white mx-2">Registrarse</a>
+            <a href="#" className="py-2 px-2 font-medium text-white rounded hover:bg-[#838E83] hover:text-white transition duration-300">Inicio</a>
+            <a href="#" className="py-2 px-2 font-medium text-white rounded hover:bg-[#838E83] hover:text-white transition duration-300">Coffee</a>
+            <a href="#" className="py-2 px-2 font-medium text-white rounded hover:bg-[#838E83] hover:text-white transition duration-300">Acerca de</a>
+            <a href="#" className="py-2 px-2 font-medium text-white rounded hover:bg-[#838E83] hover:text-white transition duration-300">Iniciar Sesión</a>
+            <a href="#" className="py-2 px-2 font-medium text-white rounded hover:bg-[#838E83] hover:text-white transition duration-300">Registrarse</a>
           </div>
         </div>
       </div>
